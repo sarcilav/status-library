9a9bbff93ef49f2fac5fc92122ef0d49443d0ad8
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2010-11-27 10:43:30 -0500

    real usage with irq

--- linux/src/so-eafit.cc
+++ linux/src/so-eafit.cc
@@ -25,7 +25,8 @@ int *GetProcessList(int *nroProcesos)
       return NULL;
     }
   int n;
-  n = (* nroProcesos) = scandir(proc_path, &procs, inside_is_a_process_dir, NULL);
+  n = (* nroProcesos) = scandir(proc_path, &procs, inside_is_a_process_dir, 
+				NULL);
   int *ans = new int[n] ;
   for(int i = 0; i < n ; ++i)
     ans[i] = atoi(procs[i] -> d_name);
@@ -33,21 +34,28 @@ int *GetProcessList(int *nroProcesos)
 }
 float CpuUsage()
 {
-  static int user = 0 , nice = 0, sys = 0, idle = 0;
-  int a_user, a_nice, a_sys, a_idle, du, dn, ds, di;
+  static int user = 0 , nice = 0, sys = 0, idle = 0, iowait = 0, irq = 0, 
+    softirq = 0;
+  int a_user, a_nice, a_sys, a_idle, a_iowait, a_irq, a_softirq,
+          du,     dn,    ds,     di,     diow,  dirq,     dsirq;
   int total = 0;
   freopen("/proc/stat", "r", stdin);
-  scanf("cpu %d %d %d %d", &a_user, &a_nice, &a_sys, &a_idle);
+  scanf("cpu %d %d %d %d %d %d %d", &a_user, &a_nice, &a_sys, &a_idle, 
+	&a_iowait, &a_irq, &a_softirq);
   fclose(stdin);
   du = a_user - user;
   dn = a_nice - nice;
   ds = a_sys - sys;
   di = a_idle - idle;
-  total += du + dn + ds + di;
+  diow = a_iowait - iowait;
+  dirq = a_irq - irq;
+  dsirq = a_softirq - softirq;
+  total = du + dn + ds + di + diow + dirq +  dsirq;
   if(total == 0)
     return -1;
-  user = a_user, nice = a_nice, sys = a_sys, idle = a_idle;
-  float usage = du + dn + ds;
+  user = a_user, nice = a_nice, sys = a_sys, idle = a_idle , 
+    iowait = a_iowait, irq = a_irq, softirq = a_softirq;
+  float usage = du + dn + ds + dirq + dsirq;
   return usage / (float) total;
 }
 
